121c71fe8841c223b676de80efde75fc631c9d9a
Mark Horninger <13180040+spam-n-eggs@users.noreply.github.com>
2019-01-22 17:37:34 -0500

    Added Connection.php (#1)

---
+++ src/Mhorninger/SQLite/Connection.php
@@ -0,0 +1,23 @@
+<?php
+namespace Mhorninger\SQLite;
+
+use Vectorface\MySQLite\MySQLite;
+
+
+class Connection extends \Illuminate\Database\Connection
+{
+     /**
+     * Create a new database connection instance.
+     *
+     * @param  \PDO|\Closure     $pdo
+     * @param  string   $database
+     * @param  string   $tablePrefix
+     * @param  array    $config
+     * @return void
+     */
+    public function __construct($pdo, $database = '', $tablePrefix = '', array $config = [])
+    {
+        parent::__construct($pdo, $database, $tablePrefix, $config);
+        $this->pdo = MySQLite::createFunctions($this->pdo);
+    }
+}
