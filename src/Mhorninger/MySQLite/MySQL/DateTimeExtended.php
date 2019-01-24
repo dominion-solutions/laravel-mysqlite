7879434dc900fb3f71be3815e40ac309fd59ea16
Mark Horninger <mhorninger@trafficcast.com>
2019-01-24 09:17:19 -0500

    Attempting to add the SECOND constant

---
+++ src/Mhorninger/MySQLite/MySQL/DateTimeExtended.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mhorninger\MySQLite\MySQL;
+
+trait DateTimeExtended
+{
+    /**
+     * 
+     */
+    function mysql_SECOND()
+    {
+        return 0;
+    }
+}
--- src/Mhorninger/MySQLite/MySQLite.php
+++ src/Mhorninger/MySQLite/MySQLite.php
@@ -1,7 +1,8 @@
 <?php
 namespace Mhorninger\MySQLite;
+use \Mhorninger\MySQLite\MySQL\DateTimeExtended;
 
 class MySQLite extends \Vectorface\MySQLite\MySQLite
 {
-
+    use DateTimeExtended
 }
