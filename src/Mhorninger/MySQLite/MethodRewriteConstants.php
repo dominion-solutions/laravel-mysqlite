f9b5084d27aca99bc498ce3b9aec714b4bdf1aa1
Mark Horninger <13180040+spam-n-eggs@users.noreply.github.com>
2019-02-13 10:13:18 -0500

    Apply fixes from StyleCI (#27)

--- src/Mhorninger/MySQLite/MethodRewriteConstants.php
+++ src/Mhorninger/MySQLite/MethodRewriteConstants.php
@@ -1,18 +1,19 @@
 <?php
+
 namespace Mhorninger\MySQLite;
 
 class MethodRewriteConstants
 {
     const METHOD_REPLACEMENTS = [
-        "/(DATE_ADD)(?=.*?, INTERVAL.*?\\))/" => 'datetime',
-        "/(?<=datetime.{13}, )INTERVAL (?=.*?\\))/" => '\'+',
-        "/(?<=datetime.{22}, )INTERVAL (?=.*?\\))/" => '\'+',
-        "/SECOND(?=\\))/" => 'seconds\'',
-        "/MINUTE(?=\\))/" => 'minutes\'',
-        "/HOUR(?=\\))/" => 'hours\'',
-        "/DAY(?=\\))/" => 'days\'',
-        "/WEEK(?=\\))/" => 'weeks\'',
-        "/MONTH(?=\\))/" => 'months\'',
-        "/YEAR(?=\\))/" => 'years\'',
+        '/(DATE_ADD)(?=.*?, INTERVAL.*?\\))/' => 'datetime',
+        '/(?<=datetime.{13}, )INTERVAL (?=.*?\\))/' => '\'+',
+        '/(?<=datetime.{22}, )INTERVAL (?=.*?\\))/' => '\'+',
+        '/SECOND(?=\\))/' => 'seconds\'',
+        '/MINUTE(?=\\))/' => 'minutes\'',
+        '/HOUR(?=\\))/' => 'hours\'',
+        '/DAY(?=\\))/' => 'days\'',
+        '/WEEK(?=\\))/' => 'weeks\'',
+        '/MONTH(?=\\))/' => 'months\'',
+        '/YEAR(?=\\))/' => 'years\'',
     ];
 }
